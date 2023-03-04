be2ede44bdc2ceae7b34cfef67a1153fab71bba4
Anrmnk <anrmnk@gmail.com>
2023-03-04 23:43:14 +0300

    15 задача работает

--- task15/Program.cs
+++ task15/Program.cs
@@ -4,16 +4,46 @@
 // 7 -> да
 // 1 -> нет
 
+// Console.WriteLine ("Введите цифру дня недели" + " ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if ( number <= 7)
+// if (number == 1 || number == 2 || number == 3 || number == 4 || number == 5)
+// {
+//     Console.WriteLine ("Не является выходным");
+// }
+// else
+// {
+//     Console.WriteLine ("Является выходным");
+// }
+// else
+// {
+//     Console.WriteLine ("Такого дня недели нет");
+// }
+
+// Задача с указанием дней недели:
+
 Console.WriteLine ("Введите цифру дня недели" + " ");
 int number = Convert.ToInt32(Console.ReadLine());
 if ( number <= 7)
 if (number == 1 || number == 2 || number == 3 || number == 4 || number == 5)
 {
-    Console.WriteLine ("Не является выходным");
+    if (number == 1)
+    Console.WriteLine ("Понедельник не является выходным");
+    if (number == 2)
+    Console.WriteLine ("Вторник не является выходным");
+    if (number == 3)
+    Console.WriteLine ("Среда не является выходным");
+    if (number == 4)
+    Console.WriteLine ("Четверг не является выходным");
+    if (number == 5)
+    Console.WriteLine ("Пятница не является выходным, но уже приятнее :)");
 }
 else
 {
-    Console.WriteLine ("Является выходным");
+   if (number == 6)
+    Console.WriteLine ("Суббота является выходным");
+    if (number == 7)
+    Console.WriteLine ("Воскресенье является выходным");
 }
 else
 {
